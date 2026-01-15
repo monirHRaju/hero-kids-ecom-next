ebe4832a6e47f05acbdda8bdbdae2fb60d943992
monirHRaju <monirhraju@gmail.com>
2026-01-15 10:11:57 +0600

    nav links with active designed

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Logo from "./Logo";
-
+import NavLink from "../buttons/NavLink";
+import Link from "next/link";
+import { CiShoppingCart } from "react-icons/ci";
 const Navbar = () => {
+    const navlinks = <>
+      <li><NavLink href="/">Home</NavLink></li>
+      <li><NavLink href="/products">Products</NavLink></li>
+      <li><NavLink href="/about">About</NavLink></li>
+    </>
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -27,52 +34,19 @@ const Navbar = () => {
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navlinks}
           </ul>
         </div>
         <Logo />
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+            {navlinks}
         </ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+      <div className="navbar-end space-x-4">
+        <Link href={'/cart'} className="btn btn-primary" ><CiShoppingCart /></Link>
+        <Link href={'/login'} className="btn btn-primary btn-outline">Login</Link>
       </div>
     </div>
   );
